2368cbc37cc811f0788bf384596615e58b5bb2af
504-23\cre <a>
2025-07-26 17:02:01 +0900

    abigailの描画

--- poy poy/src/Game/Game01/Player_abigail.h
+++ poy poy/src/Game/Game01/Player_abigail.h
@@ -1,1 +1,20 @@
-﻿
+﻿#pragma once
+#include <System/Scene.h>
+
+namespace Game01
+{
+	USING_PTR( Player_Abigail );
+
+	class Player_Abigail : public Object
+	{
+	public:
+		BP_OBJECT_DECL( Player_Abigail, u8"Game01 の Player Abigail" );
+
+		bool Init() override;
+
+		void Update() override;
+
+	private:
+	};
+
+}	 // namespace Game01
