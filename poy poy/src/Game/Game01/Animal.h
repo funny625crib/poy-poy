5fe9a6f00d0f4f5021f31760d4a0c6ef7830502b
504-23\cre <a>
2025-07-26 19:31:42 +0900

    動物がランダムに生成できるし、アニメーションも着きました

---
+++ poy poy/src/Game/Game01/Animal.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <System/Scene.h>
+
+namespace Game01
+{
+	USING_PTR( Animal );
+
+	class Animal : public Object
+	{
+	public:
+		BP_OBJECT_DECL( Animal, u8"Game01 の Animal" );
+
+		bool Init() override;
+
+		void Update() override;
+
+	private:
+	};
+
+}	 // namespace Game01
