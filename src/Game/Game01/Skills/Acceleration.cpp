9cec8f648fd2687befc44b6617cf9a54cd65f39a
Fukushima Takeru <oca2402sg0016@edu.oca.ac.jp>
2025-08-27 14:43:45 +0900

    持ち上げ、投げ、完成

---
+++ src/Game/Game01/Skills/Acceleration.cpp
@@ -0,0 +1,17 @@
+﻿#include "Acceleration.h"
+
+namespace Game01 {
+void Acceleration::Init()
+{
+    __super::Init();
+
+    // カメラオブジェクト
+    //auto camera = Scene::Object::Create<Object>();
+}
+
+void Acceleration::Update()
+{
+    __super::Update();
+    auto owner = GetOwner();
+}
+}    // namespace Game01
---
+++ src/Game/Game01/Skills/Acceleration.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <System/Scene.h>
+
+#include <System/Component/Component.h>
+
+namespace Game01 {
+USING_PTR(Acceleration);
+
+class Acceleration : public Component
+{
+public:
+    BP_COMPONENT_DECL(Acceleration, u8"Game01 の Acceleration");
+
+    void Init() override;
+
+    void Update() override;
+
+private:
+};
+
+}    // namespace Game01
