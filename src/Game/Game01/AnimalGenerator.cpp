fe935a7854f4f51a44297cfce205dcab3207d78d
504-09\cre <a>
2025-10-25 19:38:16 +0900

    アニマルの生成が完成この後出現の方法を実装

---
+++ src/Game/Game01/AnimalGenerator.cpp
@@ -0,0 +1,24 @@
+﻿#include "AnimalGenerator.h"
+#include "Animal.h"
+#include <Game/Component/State/StatePhysics.h>
+bool Game01::Generator::Init()
+{
+    SetName("Generator");
+    SetTranslate({112.0f, 50.0f, 55.0f});
+    //SetRotationAxisXYZ({0.0f, 180.0f, 0.0f});
+
+    return false;
+}
+
+void Game01::Generator::Update()
+{
+    generation_time_++;
+    if(generation_time_ >= 300.0f) {
+        auto obj = Scene::Object::Create<Animal>();
+        obj->SetTranslate({112.0f, 50.0f, 55.0f});
+        auto physics = obj->GetComponent<StatePhysics>();
+        physics->SetStatic(true);
+        obj->Game01::Animal::Throw();
+        generation_time_ = 0.0f;
+    }
+}
---
+++ src/Game/Game01/AnimalGenerator.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <System/Scene.h>
+
+namespace Game01 {
+USING_PTR(Generator);
+
+class Generator : public Object
+{
+public:
+    BP_OBJECT_DECL(Generator, u8"Game01 の Generator");
+
+    bool Init() override;
+    void Update() override;
+
+private:
+    float generation_time_ = 0.0f;
+    float speed_           = 1.0f;
+};
+
+}    // namespace Game01
