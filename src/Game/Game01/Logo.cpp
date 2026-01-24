c0b0c948df1a28d3a0ced76fed185183048ffecd
504-24\cre <a>
2026-01-24 18:43:20 +0900

    プレイヤーのロゴを追加（訂正の訂正版）

--- src/Game/Game01/Logo.cpp
+++ src/Game/Game01/Logo.cpp
@@ -7,19 +7,19 @@ bool Logo::Init()
 
     Logo_abigail_image = LoadGraph("data/Game/Logo/img_abigail.png");
     Logo_abigail.x     = 0;
-    Logo_abigail.y     = 600;
+    Logo_abigail.y     = 590;
 
     Logo_betty_image = LoadGraph("data/Game/Logo/img_betty.png");
-    Logo_betty.x     = 1060;
-    Logo_betty.y     = 600;
+    Logo_betty.x     = 960;
+    Logo_betty.y     = 590;
 
     Logo_rise_image = LoadGraph("data/Game/Logo/img_rise.png");
-    Logo_rise.x     = 760;
-    Logo_rise.y     = 600;
+    Logo_rise.x     = 640;
+    Logo_rise.y     = 590;
 
     Logo_sol_image = LoadGraph("data/Game/Logo/img_sol.png");
-    Logo_sol.x     = 460;
-    Logo_sol.y     = 600;
+    Logo_sol.x     = 320;
+    Logo_sol.y     = 590;
 
     return true;
 }
