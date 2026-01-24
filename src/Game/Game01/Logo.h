a411de2668bb02ff10cc3de2e1ef5cf48fb6e344
504-24\cre <a>
2026-01-24 19:21:33 +0900

    プレイヤー名とNPC名の上に PLAYERかNPCを入れておきました。

--- src/Game/Game01/Logo.h
+++ src/Game/Game01/Logo.h
@@ -17,15 +17,20 @@ public:
 private:
     int    Logo_abigail_image;
     float2 Logo_abigail;
+    //  プレイヤーかＮＰＣかをわかりやすくする
+    float2 name_abigail_pos;
 
     int    Logo_betty_image;
     float2 Logo_betty;
+    float2 name_betty_pos;
 
     int    Logo_rise_image;
     float2 Logo_rise;
+    float2 name_rise_pos;
 
     int    Logo_sol_image;
     float2 Logo_sol;
+    float2 name_sol_pos;
 };
 
 }    // namespace Game01
